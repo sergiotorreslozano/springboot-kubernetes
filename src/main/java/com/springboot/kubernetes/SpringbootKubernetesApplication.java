ab1a23fb4be32798ea8074d543617af9705ac0bc
sergiotorreslozano <sergiotorreslozano@gmail.com>
2017-03-21 16:48:46 +0200

    removing second discovery client to force kubernetes one

--- src/main/java/com/springboot/kubernetes/SpringbootKubernetesApplication.java
+++ src/main/java/com/springboot/kubernetes/SpringbootKubernetesApplication.java
@@ -2,11 +2,9 @@ package com.springboot.kubernetes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 @EnableFeignClients
 public class SpringbootKubernetesApplication {
 
